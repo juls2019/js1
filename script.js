62428e3c6c3628b709e313eb73f23bec3b830d8f
juls <jtsesarenko@gmail.com>
2020-04-06 19:20:44 +0300

    home work for lesson 1

---
+++ script.js
@@ -0,0 +1,12 @@
+// 1
+var tempCel = prompt('enter the temperature in Celcius please', '38');
+var tempFar = (9 / 5) * tempCel + 32;
+alert('Temperature in Farenheit is: ' + tempFar);
+// 2
+var admin;
+var name = 'Vasiliy';
+admin = name;
+alert(admin);
+// 3
+alert(1000 + '108');
+
